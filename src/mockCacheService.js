ab7f1310b13ad28d87213d487984963065d45e2c
ttsukagoshi <taro.tsukagoshi@gmail.com>
2022-07-09 01:56:23 +0900

    Create mock CacheService

---
+++ src/mockCacheService.js
@@ -0,0 +1,49 @@
+/**
+ * Classes to mock classes related to cache service
+ * @see https://developers.google.com/apps-script/reference/cache?hl=en
+ */
+
+/**
+ * CacheService class
+ * @see https://developers.google.com/apps-script/reference/cache/cache-service?hl=en
+ */
+class MockCacheService {
+  static getDocumentCache() {
+    return new MockCache();
+  }
+  static getScriptCache() {
+    return new MockCache();
+  }
+  static getUserCache() {
+    return new MockCache();
+  }
+}
+
+/**
+ * Cache class
+ * @see https://developers.google.com/apps-script/reference/cache/cache?hl=en
+ */
+class MockCache {
+  constructor() {
+    this.cache = {};
+    this.expirationInSec = 3600;
+  }
+  /**
+   * @param {String} cacheKey
+   * @returns {String}
+   */
+  get(cacheKey) {
+    return this.cache[cacheKey] || null;
+  }
+  /**
+   * @param {String} cacheKey
+   * @param {String} cacheValue
+   * @param {Number} expirationInSec
+   */
+  put(cacheKey, cacheValue, expirationInSec) {
+    this.cache[cacheKey] = cacheValue;
+    this.expirationInSec = expirationInSec;
+  }
+}
+
+module.exports = { MockCacheService };
